b098d9af6d57ffdee0ec034f9ba301cfdae9b2d5
SharpeCode57 <67698295+SharpeCode57@users.noreply.github.com>
2020-08-21 15:46:34 +0100

    Add two way binding, Add styling and in-line styles

--- src/App.js
+++ src/App.js
@@ -25,27 +25,45 @@ const App = props => {
     })
   }
 
+  const nameChangedHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
+  const style = {
+    backgroundColor: 'White',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
 
   return (
     <div className="App">
       <h1>Hi, I'm a react app!</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button
+        style={style}
+        onClick={switchNameHandler}>Switch Name</button>
       <Person
         name={personsState.persons[0].name}
         age={personsState.persons[0].age} />
       <Person
         name={personsState.persons[1].name}
         age={personsState.persons[1].age}
-        click={switchNameHandler} >My hobbies: Racing</Person>
+        click={switchNameHandler}
+        changed={nameChangedHandler} >My hobbies: Racing</Person>
       <Person
         name={personsState.persons[2].name}
         age={personsState.persons[2].age} />
     </div>
   );
 }
-
-
 
 export default App;
 
